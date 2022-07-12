f693342e1a34e0e95f7bc624ff92e0f0485146b5
Michael Junges <michael.junges@develcode.com.br>
2022-07-12 10:07:35 -0300

    mais correções de aspas

--- src/components/CategoryButton/styles.ts
+++ src/components/CategoryButton/styles.ts
@@ -1,10 +1,7 @@
-import { RectButton } from "react-native-gesture-handler";
-import { RFValue } from "react-native-responsive-fontsize";
-import styled from "styled-components/native";
+import {RFValue} from 'react-native-responsive-fontsize';
+import styled from 'styled-components/native';
 
-export const View = styled.View`
-
-`;
+export const View = styled.View``;
 
 export const Container = styled.TouchableOpacity`
   width: ${RFValue(99)}px;
@@ -13,10 +10,10 @@ export const Container = styled.TouchableOpacity`
   margin-right: ${RFValue(8)}px;
   justify-content: center;
   align-items: center;
-  background-color: ${({ theme }) => theme.colors.background_red};
+  background-color: ${({theme}) => theme.colors.background_red};
 `;
 
 export const Title = styled.Text`
-  color: ${({ theme }) => theme.colors.text_white};
+  color: ${({theme}) => theme.colors.text_white};
   font-size: ${RFValue(12)}px;
 `;
