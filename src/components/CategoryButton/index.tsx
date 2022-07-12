94c8d5a2ca14eb01fd9b637db4e42863370db117
Michael Junges <michael.junges@develcode.com.br>
2022-07-12 09:52:12 -0300

    remoção aspas duplas do category button

--- src/components/CategoryButton/index.tsx
+++ src/components/CategoryButton/index.tsx
@@ -1,25 +1,18 @@
-import React, { useEffect, useState } from "react";
-import { StyleSheet, TextProps, TouchableOpacityProps } from "react-native";
-import { RectButtonProps } from "react-native-gesture-handler";
-import { RFValue } from "react-native-responsive-fontsize";
-import { useAuth } from "../../global/Context";
-import { useFetch } from "../../global/services/get";
-import theme from "../../global/styles/theme";
-import { Container, Title, View } from "./styles";
-
+import React from 'react';
+import {TextProps} from 'react-native';
+import {RectButtonProps} from 'react-native-gesture-handler';
+import {Container, Title} from './styles';
 interface Props {
   title: string;
   onPress: () => void;
-  style: RectButtonProps["style"];
-  textStyle: TextProps["style"];
+  style: RectButtonProps['style'];
+  textStyle: TextProps['style'];
 }
 
-export function Category({ title, style, textStyle, onPress, ...rest }: Props) {
+export function Category({title, style, textStyle, onPress, ...rest}: Props) {
   return (
     <Container onPress={onPress} activeOpacity={1} {...rest} style={style}>
       <Title style={textStyle}>{title}</Title>
     </Container>
   );
 }
-
-
